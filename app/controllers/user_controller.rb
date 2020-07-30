acc8ec0af800cc256bf268df2a914f871ea2255c
guerino antonini <guerantonini94@gmail.com>
2020-07-29 21:20:31 -0400

    WIP- Made user be able in signup, and login, as well as a few anchors from homepage.

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -5,14 +5,40 @@ class UserController < ApplicationController
     end
 
     post '/users/signup' do 
-        @user = User.create(username: params[:username], email: params[:email], password_digest: params[:password_digest])
-        redirect "/users/#{@user.id}"
+
+        if params[:username] == "" && params[:password] = ""
+            redirect "/users/signup"
+        else
+            @user = User.create(
+                username: params[:username], 
+                email: params[:email], 
+                password: params[:password])
+
+            session[:user_id] = @user_id
+            redirect "/users/#{@user.id}"
+        end
+    end
+
+    get '/users/login' do 
+        erb :'/users/login'
+    end
+
+    post '/users/login' do
+        @user = User.find_by(username: params[:username])
+
+        if @user && @user.authenticate(params[:password])
+            session[:user_id] = @user_id
+            redirect "/users/#{@user.id}"
+        else
+            redirect "/users/login"
+        end
     end
 
     get '/users/:id' do
-        @user = User.find_by(params[:id])
+        @user = User.find(params[:id])
         erb :'/users/show'
     end
+
 
   
 
